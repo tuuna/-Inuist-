956e2b8dc0293c09a1cc557e9f287d04b47cf51d
vampirebitter <lj550566181@gmail.com>
2016-11-21 20:21:09 +0800

    complete

--- Mac/start.sh
+++ Mac/start.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/php
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
@@ -8,7 +9,7 @@ $curl->post('http://a.nuist.edu.cn/index.php/index/login', array(
     'username' => '15861829093',
     'domain' => 'CMCC',	//中国移动CMCC ，南京信息工程大学NUIST,中国电信ChinaNet,中国联通Unicom
     'password' => base64_encode('123321'),
-    'enablemacauth' => 0	
+    'enablemacauth' => 1	
 ));
 
 if($curl->response->status)
