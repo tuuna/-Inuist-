d551be84a0a5f0800e84fdb42060dbdb66ab4e22
laixiaojie <vampirebitter@users.noreply.github.com>
2016-11-21 20:37:42 +0800

    complete

--- Linux/start.sh
+++ Linux/start.sh
@@ -6,10 +6,10 @@ use \Curl\Curl;
 
 $curl = new Curl();
 $curl->post('http://a.nuist.edu.cn/index.php/index/login', array(
-    'username' => '15861829093',
+    'username' => '',//填入手机号码
     'domain' => 'CMCC',	//中国移动CMCC ，南京信息工程大学NUIST,中国电信ChinaNet,中国联通Unicom
-    'password' => base64_encode('123321'),
-    'enablemacauth' => 0	
+    'password' => base64_encode('123321'),//填入密码，一般都是123321
+    'enablemacauth' => 0	//mac用户填1
 ));
 
 if($curl->response->status)
